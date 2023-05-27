e6a6d2b7d2b7bf9ec6f24769a5846d94cfaedacd
Hai Dinh Tuan <haidinhtuan@gmail.com>
2023-05-27 11:00:24 +0200

    update SearchNFServiceUri

--- NFs/amf/internal/util/search_nf_service.go
+++ NFs/amf/internal/util/search_nf_service.go
@@ -12,17 +12,22 @@ func SearchNFServiceUri(nfProfile models.NfProfile, serviceName models.ServiceNa
 		for _, service := range *nfProfile.NfServices {
 			if service.ServiceName == serviceName && service.NfServiceStatus == nfServiceStatus {
 				if nfProfile.Fqdn != "" {
-					nfUri = nfProfile.Fqdn
+					//nfUri = nfProfile.Fqdn
+					nfUri = "http://127.0.0.1:9999"
 				} else if service.Fqdn != "" {
-					nfUri = service.Fqdn
+					//nfUri = service.Fqdn
+					nfUri = "http://127.0.0.1:9999"
 				} else if service.ApiPrefix != "" {
-					nfUri = service.ApiPrefix
+					//nfUri = service.ApiPrefix
+					nfUri = "http://127.0.0.1:9999"
 				} else if service.IpEndPoints != nil {
 					point := (*service.IpEndPoints)[0]
 					if point.Ipv4Address != "" {
-						nfUri = getSbiUri(service.Scheme, point.Ipv4Address, point.Port)
+						//nfUri = getSbiUri(service.Scheme, point.Ipv4Address, point.Port)
+						nfUri = "http://127.0.0.1:9999"
 					} else if len(nfProfile.Ipv4Addresses) != 0 {
-						nfUri = getSbiUri(service.Scheme, nfProfile.Ipv4Addresses[0], point.Port)
+						//nfUri = getSbiUri(service.Scheme, nfProfile.Ipv4Addresses[0], point.Port)
+						nfUri = "http://127.0.0.1:9999"
 					}
 				}
 			}
